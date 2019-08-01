8896a0dc4aee57d29d46fa62d7cbec8638d1f11b
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-01 16:19:32 -0300

    Criado script para classe de controle remoto do robô.

---
+++ controle_remoto.py
@@ -0,0 +1,16 @@
+#-*- coding utf-8 -*-
+
+# ------------------ Projeto Atena -----------------------
+
+# 	Autores: Estanislau Filho e José Antônio
+# 	Ano: 2019
+# 	Orientadora: Natália Batista
+#	Instituição: CEFET-MG	
+# 	Link github: https://github.com/EstanislauFilho/Atena
+
+#	Classe: Main
+
+# --------------------------------------------------------
+
+class Controle_Remoto:
+
