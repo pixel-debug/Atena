4f74f156fb46b440db64c9e95289cabad4803dbf
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-01 17:54:16 -0300

    Criado loop para captura e armazenamento dos quadros em um vetor.

--- camera.py
+++ camera.py
@@ -24,4 +24,8 @@ camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
 
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	#O vetor com os frames capturados sao armazenados no vetor image	
+	image = frame.array
+	
 
