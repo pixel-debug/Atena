ae8e8149cf89a8856aa0f7571250a4e96fc264cd
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-01 10:45:11 -0300

    Criado script para classe dos motores.

---
+++ Motores.py
@@ -0,0 +1,14 @@
+#-*- coding utf-8 -*-
+
+# ------------------ Projeto Atena ---------------------
+# Autores: Estanislau Filho
+# Ano: 2019	
+# Link github: https://github.com/EstanislauFilho/Atena
+# ------------------------------------------------------
+
+import RPi.GPIO as GPIO
+import Variaveis as var
+
+class Motores:
+
+	def __init__(self):
