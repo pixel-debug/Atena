a1b5cfafc457c0fa25d1e8c0ae00f26047184250
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-01 10:32:16 -0300

    Definido variáveis para motor da esquerda.

--- Variaveis.py
+++ Variaveis.py
@@ -9,4 +9,6 @@ class Variaveis:
 		self.pin_motor_direita_frente = 19
 		self.pin_motor_direita_tras = 26
 
-
+		self.pin_pwm_motor_esquerda = 12
+		self.pin_motor_esquerda_frente = 16
+		self.pin_motor_esquerda_tras = 20
