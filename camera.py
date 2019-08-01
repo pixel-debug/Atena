d983f1509efb2cf0c9db123cba85d0fe45214fe0
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-01 17:56:41 -0300

    Obtenção do ponto central da imagem e rotação de 180º da imagem dado o posicionamento da camera no robô.

--- camera.py
+++ camera.py
@@ -25,7 +25,18 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	#O vetor com os frames capturados sao armazenados no vetor image	
+	# O vetor com os frames capturados sao armazenados no vetor image	
 	image = frame.array
 	
+	# Armazenamento das dimensoes padrao dos frames a serem capturados e criacao do vetor com os pontos centrais da imagem
+	(h, w) = image.shape[:2]
+	center = (w / 2, h / 2)
 
+	# Utilizacao da funcao RotationMatrix2D para realizar a rotacao da imagem a partir do ponto central
+	M = cv2.getRotationMatrix2D(center, 180, 1.0)
+
+	# Realiza a rotacao da imagem a partir das funcoes determinadas acima
+	rotated = cv2.warpAffine(image, M, (w, h))
+
+
+	
