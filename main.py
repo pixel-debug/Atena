845652eb0a76338efbddb5c6761f7513cbc15c1f
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-01 11:30:44 -0300

    Configurações básicas dos GPIO's.

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+#-*- coding utf-8 -*-
+
+# ------------------ Projeto Atena -----------------------
+
+# 	Autores: Estanislau Filho e José Antônio
+# 	Ano: 2019
+# 	Orientadora: Natália Batista
+#	Instituição: CEFET-MG	
+# 	Link github: https://github.com/EstanislauFilho/Atena
+
+#	Classe: Main
+
+# --------------------------------------------------------
+
 import RPi.GPIO as GPIO
+import Variaveis as var
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setwarnings(False)
 
 class main:
 
