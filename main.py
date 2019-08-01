cdc80d86395c95edfd0f7dbe3033c88642a641fc
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-01 11:45:16 -0300

    Teste básico de movimento para frente e para trás no intervalo de 2 segundos.

--- main.py
+++ main.py
@@ -33,8 +33,17 @@ controle_velocidade_esquerda.start(0)
 class main:
 	try:
 		while (True):
-		
+			motor.movimento_frente(var.velocidade, controle_velocidade_direita, controle_velocidade_esquerda)
+			time.sleep(2)
 
+			motor.parar_movimento(controle_velocidade_direita, controle_velocidade_esquerda)
+			time.sleep(2)
+
+			motor.movimento_tras(var.velocidade, controle_velocidade_direita, controle_velocidade_esquerda)
+			time.sleep(2)
+
+			motor.parar_movimento(controle_velocidade_direita, controle_velocidade_esquerda)
+			time.sleep(2)
 	finally:
 		print("Cleaning up")
 		GPIO.cleanup()
