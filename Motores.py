435eb03633b8022b69b659ec17807e224c325f27
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-01 10:55:48 -0300

    Criado função para movimentar para trás.

--- Motores.py
+++ Motores.py
@@ -23,3 +23,13 @@ class Motores:
 		GPIO.output(var.pin_motor_esquerda_frente, True)
 		GPIO.output(var.pin_motor_esquerda_tras, False)
 		controle_velocidade_esquerda.ChangeDutyCycle(velocidade)
+
+
+	def movimento_tras(velocidade, controle_velocidade_direita, controle_velocidade_esquerda):
+		GPIO.output(var.pin_motor_direita_frente, False)
+		GPIO.output(var.pin_motor_direita_tras, True)
+		controle_velocidade_direita.ChangeDutyCycle(velocidade)
+
+		GPIO.output(var.pin_motor_esquerda_frente, False)
+		GPIO.output(var.pin_motor_esquerda_tras, True)
+		controle_velocidade_esquerda.ChangeDutyCycle(velocidade)
