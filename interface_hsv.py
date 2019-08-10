d592798b8fef1f64fd3dc94a8f5a55a7b2d67184
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-10 20:32:07 -0300

    Criado função para rotacionar imagem 180º dado o posicionamento da Picamera.

--- interface_hsv.py
+++ interface_hsv.py
@@ -16,15 +16,30 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+import numpy as np
 
 # Inicializacao da camera e parâmetros de resolucao e quadros por segundo capturado
 camera = PiCamera()
-camera.resolution = (640, 480)
+camera.resolution = (480, 320)
 camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+rawCapture = PiRGBArray(camera, size=(480, 320))
 time.sleep(0.1)
 
 def nada(x): pass
+
+def rotaciona_imagem(img):
+	# Armazenamento das dimensoes dos frames e criacao do vetor com referencia aos pixels do centro da imagem
+	(h, w) = img.shape[:2]
+	centro_imagem = (w / 2, h / 2)
+
+	# Utilizacao da funcao RotationMatrix2D para realizar a rotacao da imagem a partir do ponto central
+	M = cv2.getRotationMatrix2D(centro_imagem, 180, 1.0)
+
+	# Realiza a rotacao da imagem a partir das funcoes determinadas acima
+	img_rotacionada = cv2.warpAffine(img, M, (w, h))
+
+	return img_rotacionada
+
 
 # -------------------------- Interface HSV ------------------------------------
 cv2.namedWindow("Interface_HSV")
@@ -48,33 +63,28 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 	# Obtencao dos valores maximos e minimos dos parametros do HSV definidos pelas barras na interface
 	val_minimo_H = cv2.getTrackbarPos("Valor Minimo - H", "Interface_HSV")
-    val_minimo_S = cv2.getTrackbarPos("Valor Minimo - S", "Interface_HSV")
-    val_minimo_V = cv2.getTrackbarPos("Valor Minimo - V", "Interface_HSV")
-    
-    val_maximo_H = cv2.getTrackbarPos("Valor Maximo - H", "Interface_HSV")
-    val_maximo_S = cv2.getTrackbarPos("Valor Maximo - S", "Interface_HSV")
-    val_maximo_V = cv2.getTrackbarPos("Valor Maximo - V", "Interface_HSV")
+	val_minimo_S = cv2.getTrackbarPos("Valor Minimo - S", "Interface_HSV")
+	val_minimo_V = cv2.getTrackbarPos("Valor Minimo - V", "Interface_HSV")
+
+	val_maximo_H = cv2.getTrackbarPos("Valor Maximo - H", "Interface_HSV")
+	val_maximo_S = cv2.getTrackbarPos("Valor Maximo - S", "Interface_HSV")
+	val_maximo_V = cv2.getTrackbarPos("Valor Maximo - V", "Interface_HSV")
 
 
 	valores_maximos_hsv = np.array([val_maximo_H, val_maximo_S, val_maximo_V])
-    valores_minimos_hsv = np.array([val_minimo_H, val_minimo_S, val_minimo_V])
-    mascara = cv2.inRange(imagem_hsv, valores_minimos_hsv, valores_maximos_hsv)
-    imagem_resultado = cv2.bitwise_and(imagem, imagem, mask=mascara)
+	valores_minimos_hsv = np.array([val_minimo_H, val_minimo_S, val_minimo_V])
+	mascara = cv2.inRange(imagem_hsv, valores_minimos_hsv, valores_maximos_hsv)
+	imagem_hsv = cv2.bitwise_and(imagem, imagem, mask=mascara)
 
 
-	# Armazenamento das dimensoes dos frames e criacao do vetor com referencia aos pixels do centro da imagem
-	(h, w) = imagem.shape[:2]
-	centro_imagem = (w / 2, h / 2)
+	imagem_rotacionada = rotaciona_imagem(imagem)
 
-	# Utilizacao da funcao RotationMatrix2D para realizar a rotacao da imagem a partir do ponto central
-	M = cv2.getRotationMatrix2D(centro_imagem, 180, 1.0)
-
-	# Realiza a rotacao da imagem a partir das funcoes determinadas acima
-	imagem_rotacionada = cv2.warpAffine(imagem, M, (w, h))
-
+	imagem_hsv = rotaciona_imagem(imagem_hsv)
 
 	# Apresentacao dos frames em tempo real
 	cv2.imshow("Streaming Camera Atena", imagem_rotacionada)
+
+	cv2.imshow('Interface HSV Atena', imagem_hsv)
 
 	# Faz a limpeza do stream e faz a preparacao para a captura dos proximos frames
 	rawCapture.truncate(0)
