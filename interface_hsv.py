7edb86eca4cc7c7ee869d10fa187c94d046ed8e1
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-10 19:05:46 -0300

    Laço de repetição e armazenamento dos frames no vetor imagem.

--- interface_hsv.py
+++ interface_hsv.py
@@ -23,3 +23,8 @@ camera.resolution = (800, 600)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(800, 600))
 time.sleep(0.1)
+
+
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# O vetor com os frames capturados sao armazenados no vetor image	
+	imagem = frame.array
