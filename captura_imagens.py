995364ba3165cebfed68cd7d23ba45b8c8ed1d9a
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-11 17:39:53 -0300

    Adicionado método para limpeza dos frames capturados.

--- captura_imagens.py
+++ captura_imagens.py
@@ -20,9 +20,9 @@ import numpy as np
 
 # Inicializacao da camera e parâmetros de resolucao e quadros por segundo capturado
 camera = PiCamera()
-camera.resolution = (480, 320)
+camera.resolution = (640, 480)
 camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(480, 320))
+rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
 
 
@@ -48,18 +48,23 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	# O vetor com os frames capturados sao armazenados no vetor image	
 	imagem = frame.array
 
+
 	if cv2.waitKey(1) & 0xFF == ord('c'):
-		salva_imagem = cv2.resize(imagem[y:y + h, x:x + w], (largura_imagem, altura_imagem))
-		salva_imagem = rotaciona_imagem(salva_imagem)
+		salva_imagem = rotaciona_imagem(imagem)
+		#salva_imagem = cv2.resize(salva_imagem[y:y + h, x:x + w], (largura_imagem, altura_imagem))
 		cv2.imwrite("Imagens/" + str(cont_imagem) + ".jpg",salva_imagem)
 		print(str(cont_imagem)+"º imagem capturada com sucesso! Pressione 'ESC' para encerrar...")
 		cont_imagem += 1    
-		
 	
-	cv2.imshow("Streaming Camera Atena", saçva_imagem)
+	imagem_rotacionada = rotaciona_imagem(imagem)
+	
+	cv2.imshow("Streaming Camera Atena", imagem_rotacionada)
 
-    if cv2.waitKey(1) & 0xFF == 27:
-        break
+	# Faz a limpeza do stream e faz a preparacao para a captura dos proximos frames
+	rawCapture.truncate(0)
+
+	if cv2.waitKey(1) & 0xFF == 27:
+		break
 
 cv2.destroyAllWindows()
 
