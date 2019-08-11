8d41cfc7c16b01cd8ae4f4b2ff4e7bc76e04fe5d
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-10 21:40:18 -0300

    Criado script para capturar fotos para treinamento.

---
+++ captura_imagens.py
@@ -0,0 +1,19 @@
+#-*- coding utf-8 -*-
+
+# ------------------ Projeto Atena -----------------------
+
+# 	Autores: Estanislau Filho e José Antônio
+# 	Ano: 2019
+# 	Orientadora: Natália Batista
+#	Instituição: CEFET-MG	
+# 	Link github: https://github.com/EstanislauFilho/Atena
+
+#	Classe: Interface HSV
+
+# --------------------------------------------------------
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+import numpy as np
