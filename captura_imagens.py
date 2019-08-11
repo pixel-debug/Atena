246786c5922dd392182be145c189cd3ecd966818
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-11 17:40:11 -0300

    Adicionado método para limpeza dos frames capturados.

--- captura_imagens.py
+++ captura_imagens.py
@@ -51,7 +51,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 	if cv2.waitKey(1) & 0xFF == ord('c'):
 		salva_imagem = rotaciona_imagem(imagem)
-		#salva_imagem = cv2.resize(salva_imagem[y:y + h, x:x + w], (largura_imagem, altura_imagem))
+		salva_imagem = cv2.resize(salva_imagem[y:y + h, x:x + w], (largura_imagem, altura_imagem))
 		cv2.imwrite("Imagens/" + str(cont_imagem) + ".jpg",salva_imagem)
 		print(str(cont_imagem)+"º imagem capturada com sucesso! Pressione 'ESC' para encerrar...")
 		cont_imagem += 1    
