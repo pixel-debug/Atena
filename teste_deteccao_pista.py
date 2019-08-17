a3a6b3c8503bddd136116f7b91c9b8e85443faa5
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-17 17:48:12 -0300

    Inicialização da câmera, definição dos parâmetros de resolução e dos quadros capturados por segundo.

--- teste_deteccao_pista.py
+++ teste_deteccao_pista.py
@@ -11,3 +11,10 @@
 #	Script: Teste Detecção Pista
 
 # --------------------------------------------------------
+
+# Inicializacao da camera, definição dos parâmetros de resolucao e dos quadros capturados por segundo capturado
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+time.sleep(0.1)
