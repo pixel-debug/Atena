1b651f8881a24440930f668f54ab121553b4508b
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-17 18:00:31 -0300

    Criado função para rotacionar a imagem 180º dada a posição da Picamera no robô.

--- teste_deteccao_pista.py
+++ teste_deteccao_pista.py
@@ -26,11 +26,22 @@ camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
 
+# Funcao para rotacionar a imagem 180 graus
+def rotaciona_imagem(imagem):
+	(altura, largura) = imagem.shape[:2]
+	centro_imagem = (largura / 2, altura / 2)
+	matriz_imagem = cv2.getRotationMatrix2D(centro_imagem, 180, 1.0)
+	imagem_rotacionada = cv2.warpAffine(imagem, matriz_imagem, (largura, altura))
+	return imagem_rotacionada
+
 # Loop
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# O vetor com os frames capturados sao armazenados no vetor imagem	
 	imagem = frame.array
 
+	imagem = rotaciona_imagem(imagem)
+
+	# Apresentacao da imagem
 	cv2.imshow("Teste Deteccao Pista", imagem)
 
 	# Faz a limpeza do stream e faz a preparacao para a captura dos proximos frames
