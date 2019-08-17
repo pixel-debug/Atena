c1ef28fd220aff94bcc2020a0c7c6849ebe8c163
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-17 17:50:00 -0300

    Criado loop.

--- teste_deteccao_pista.py
+++ teste_deteccao_pista.py
@@ -12,9 +12,15 @@
 
 # --------------------------------------------------------
 
-# Inicializacao da camera, definição dos parâmetros de resolucao e dos quadros capturados por segundo capturado
+# Inicializacao da camera, definicao dos parametros de resolucao e dos quadros capturados por segundo capturado
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
 time.sleep(0.1)
+
+# Loop
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+
+
+cv2.destroyAllWindows()
