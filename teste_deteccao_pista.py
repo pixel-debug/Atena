840b130951df5689f3283eb06b47847679084785
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-18 09:36:35 -0300

    Alterado a função de região de interesse para obter uma imagem sobre perspectiva focando só a pista.

--- teste_deteccao_pista.py
+++ teste_deteccao_pista.py
@@ -29,22 +29,26 @@ time.sleep(0.1)
 ponto_pista_1, ponto_pista_2, ponto_pista_3, ponto_pista_4 = (48,208), (255,208), (29,235), (276,235), 
 ponto_destino_1, ponto_destino_2, ponto_destino_3, ponto_destino_4 = (85,0), (220,0), (85,240), (220,240),
 
-pontos_pista = np.array([ponto_pista_1, ponto_pista_2, ponto_pista_3, ponto_pista_4],np.float32)
-pontos_destino = np.array([ponto_destino_1, ponto_destino_2, ponto_destino_3, ponto_destino_4],np.float32)
+pontos_pista = np.float32([[ponto_pista_1], [ponto_pista_2], [ponto_pista_3], [ponto_pista_4]])
+pontos_destino = np.float32([[ponto_destino_1], [ponto_destino_2], [ponto_destino_3], [ponto_destino_4]])
 
 # Funcao para regiao de interesse
-def regiao_de_interesse():
-	cv2.line(imagem, ponto_pista_1, ponto_pista_2, (0,0,255), 2)
-	cv2.line(imagem, ponto_pista_1, ponto_pista_3, (0,0,255), 2)
-	cv2.line(imagem, ponto_pista_2, ponto_pista_4, (0,0,255), 2)
-	cv2.line(imagem, ponto_pista_3, ponto_pista_4, (0,0,255), 2)
+def regiao_de_interesse(rg_imagem, pontos_pista, pontos_destino):
+	cv2.line(rg_imagem, ponto_pista_1, ponto_pista_2, (0,0,255), 2)
+	cv2.line(rg_imagem, ponto_pista_1, ponto_pista_3, (0,0,255), 2)
+	cv2.line(rg_imagem, ponto_pista_2, ponto_pista_4, (0,0,255), 2)
+	cv2.line(rg_imagem, ponto_pista_3, ponto_pista_4, (0,0,255), 2)
 
-	cv2.line(imagem, ponto_destino_1, ponto_destino_2, (0,255,0), 2)
-	cv2.line(imagem, ponto_destino_1, ponto_destino_3, (0,255,0), 2)
-	cv2.line(imagem, ponto_destino_2, ponto_destino_4, (0,255,0), 2)
-	cv2.line(imagem, ponto_destino_3, ponto_destino_4, (0,255,0), 2)
-	
+	cv2.line(rg_imagem, ponto_destino_1, ponto_destino_2, (0,255,0), 2)
+	cv2.line(rg_imagem, ponto_destino_1, ponto_destino_3, (0,255,0), 2)
+	cv2.line(rg_imagem, ponto_destino_2, ponto_destino_4, (0,255,0), 2)
+	cv2.line(rg_imagem, ponto_destino_3, ponto_destino_4, (0,255,0), 2)
+
 	matriz = cv2.getPerspectiveTransform(pontos_pista, pontos_destino)
+
+	rg_imagem = cv2.warpPerspective(rg_imagem, matriz, (360,240))
+
+	return rg_imagem
 
 # Funcao para rotacionar a imagem 180 graus
 def rotaciona_imagem(imagem):
@@ -61,25 +65,25 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 	imagem = rotaciona_imagem(imagem)
 
-	regiao_de_interesse()
+	imagem_perspectiva = imagem
+
+	imagem_perspectiva = regiao_de_interesse(imagem_perspectiva, pontos_pista, pontos_destino)
 
 	# Convertendo padrao de cores da imagem
-	imagem_bgr = imagem
+	#imagem_bgr = imagem
 
-	imagem_rgb = cv2.cvtColor(imagem_bgr, cv2.COLOR_BGR2RGB)
+	#imagem_rgb = cv2.cvtColor(imagem_bgr, cv2.COLOR_BGR2RGB)
 	
-
-
 	# Apresentacao da imagem
-	cv2.namedWindow("BGR", cv2.WINDOW_KEEPRATIO);
-	cv2.moveWindow("BGR", 50, 100);
-	cv2.resizeWindow("BGR", 480, 320)
-	cv2.imshow("BGR", imagem_bgr)
+	cv2.namedWindow("Deteccao Pista", cv2.WINDOW_KEEPRATIO);
+	cv2.moveWindow("Deteccao Pista", 50, 100);
+	cv2.resizeWindow("Deteccao Pista", 480, 320)
+	cv2.imshow("Deteccao Pista", imagem)
 
-	#cv2.namedWindow("RGB", cv2.WINDOW_KEEPRATIO);
-	#cv2.moveWindow("RGB", 550, 100);
-	#cv2.resizeWindow("RGB", 480, 320)
-	#cv2.imshow("RGB", imagem_rgb)
+	cv2.namedWindow("Perspectiva", cv2.WINDOW_KEEPRATIO);
+	cv2.moveWindow("Perspectiva", 550, 100);
+	cv2.resizeWindow("Perspectiva", 480, 320)
+	cv2.imshow("Perspectiva", imagem_perspectiva)
 
 
 	# Faz a limpeza do stream e faz a preparacao para a captura dos proximos frames
