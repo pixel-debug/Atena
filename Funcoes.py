93e229d5479b8a1be72feeeb3676ccd65d8da65d
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-28 16:12:38 -0300

    Criado procedimento para correção do percurso do robô com os fototransistores.

--- Funcoes.py
+++ Funcoes.py
@@ -17,12 +17,24 @@ import Variaveis as var
 import time
 
 def buzina():
-	GPIO.output(var.pin_Bozinha, True)
+	GPIO.output(var.pin_BOZINA, True)
 	time.sleep(0.2)
-	GPIO.output(var.pin_Bozinha, False)
+	GPIO.output(var.pin_BOZINA, False)
 	time.sleep(0.1)
-	GPIO.output(var.pin_Bozinha, True)
+	GPIO.output(var.pin_BOZINA, True)
 	time.sleep(0.4)
-	GPIO.output(var.pin_Bozinha, False)
+	GPIO.output(var.pin_BOZINA, False)
 	time.sleep(0.2)
+
+def correcao_fototransistor(pin_FOTOTRANSISTOR_DIR, pin_FOTOTRANSISTOR_ESQ):
+	if (GPIO.input(pin_FOTOTRANSISTOR_DIR) == GPIO.LOW and GPIO.input(pin_FOTOTRANSISTOR_ESQ) == GPIO.LOW):
+		print("frente.")
+	elif GPIO.input(pin_FOTOTRANSISTOR_DIR) == GPIO.HIGH and GPIO.input(pin_FOTOTRANSISTOR_ESQ) == GPIO.LOW:
+		print("Direito Branco.")
+	elif GPIO.input(pin_FOTOTRANSISTOR_DIR) == GPIO.LOW and GPIO.input(pin_FOTOTRANSISTOR_ESQ) == GPIO.HIGH:
+		print("Esquerdo Branco.")
+	else:
+		print("Contencao.")
+
+	time.sleep(1)
 	
