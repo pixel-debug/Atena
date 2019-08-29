89761e807c59c6c410b12ac2d86a85a5f5b040e4
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-28 21:16:32 -0300

    Importado bibliotecas e definido configurações dos pinos.

--- Scripts Testes/teste_sensor_obstaculo.py
+++ Scripts Testes/teste_sensor_obstaculo.py
@@ -11,3 +11,12 @@
 #	Script: Teste Sensor Obstaculo
 
 # --------------------------------------------------------
+
+
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setwarnings(False)
