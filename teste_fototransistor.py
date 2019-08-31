888e87202491b871c3af89e3f11d458a048cc739
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-31 19:41:27 -0300

    Criado script para teste dos fototransistores.

---
+++ teste_fototransistor.py
@@ -0,0 +1,13 @@
+#-*- coding utf-8 -*-
+
+# ------------------ Projeto Atena -----------------------
+
+# 	Autores: Estanislau Filho e José Antônio
+# 	Ano: 2019
+# 	Orientadora: Natália Batista
+#	Instituição: CEFET-MG	
+# 	Link github: https://github.com/EstanislauFilho/Atena
+
+#	Script: Teste Fototransitores
+
+# --------------------------------------------------------
