43d285a41af8686243560a4be7d8a4db123622c3
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-31 19:52:42 -0300

    Criando duas entradas para leitura dos canais 0 e 1 do conversor AD.

--- teste_fototransistor.py
+++ teste_fototransistor.py
@@ -23,3 +23,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 # Criando um objeto ADC com a interface i2c
 ads = ADS.ADS1115(i2c)
+
+# Criando duas entradas para leitura dos canais 0 e 1 do conversor AD
+fototransistor_dir = AnalogIn(ads, ADS.P0)
+fototransistor_esq = AnalogIn(ads, ADS.P1)
