3b32f190727ad1a55a6225a3ad50077e8b73c786
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-31 20:45:43 -0300

    Importação das bibliotecas e chamada da interface i2c.

---
+++ Scripts Testes/teste_fototransistor.py
@@ -0,0 +1,21 @@
+#-*- coding utf-8 -*-
+
+# ------------------ Projeto Atena -----------------------
+
+# 	Autores: Estanislau Filho e José Antônio
+# 	Ano: 2019
+# 	Orientadora: Natália Batista
+#	Instituição: CEFET-MG	
+# 	Link github: https://github.com/EstanislauFilho/Atena
+
+#	Script: Teste Fototransitores
+
+# --------------------------------------------------------
+
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+
