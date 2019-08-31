574a39085c2dd620695f867b1307c8079dcc6a9c
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-31 20:05:42 -0300

    Criado laço de repetição e obtenção dos valores obtidos com o sensor.

--- teste_fototransistor.py
+++ teste_fototransistor.py
@@ -27,3 +27,9 @@ ads = ADS.ADS1115(i2c)
 # Criando duas entradas para leitura dos canais 0 e 1 do conversor AD
 fototransistor_dir = AnalogIn(ads, ADS.P0)
 fototransistor_esq = AnalogIn(ads, ADS.P1)
+
+
+# 1º valor raw e 2º valor da tensão
+while True:
+    print("Fototransistor Direita: {:>5}\t{:>5.3f} \tFototransistor Esquerda: {:>5}\t{:>5.3f}".format(fototransistor_dir.value, fototransistor_dir.voltage, fototransistor_esq.value, fototransistor_esq.voltage))
+    time.sleep(0.5)
