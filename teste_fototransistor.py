65ef46b35abd3326a855b0082451f7bbed6bdac9
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-31 19:42:28 -0300

    Importação das bibliotecas e chamada da interface i2c.

--- teste_fototransistor.py
+++ teste_fototransistor.py
@@ -11,3 +11,12 @@
 #	Script: Teste Fototransitores
 
 # --------------------------------------------------------
+
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+# Create the I2C bus
+i2c = busio.I2C(board.SCL, board.SDA)
