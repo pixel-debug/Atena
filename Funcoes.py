6d28d8f9ff7167e3c0a563a127a6f70f620dcce9
EstanislauFilho <estanislau.sena@gmail.com>
2019-08-31 22:16:45 -0300

    Criado função para atuação dos fototransitores como identificador das faixas da pista.

--- Funcoes.py
+++ Funcoes.py
@@ -15,6 +15,10 @@
 import RPi.GPIO as GPIO
 import Variaveis as var
 import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
 def buzina():
 	GPIO.output(var.pin_BOZINA, True)
@@ -26,15 +30,18 @@ def buzina():
 	GPIO.output(var.pin_BOZINA, False)
 	time.sleep(0.2)
 
-def correcao_fototransistor(pin_FOTOTRANSISTOR_DIR, pin_FOTOTRANSISTOR_ESQ):
-	if (GPIO.input(pin_FOTOTRANSISTOR_DIR) == GPIO.LOW and GPIO.input(pin_FOTOTRANSISTOR_ESQ) == GPIO.LOW):
-		print("frente.")
-	elif GPIO.input(pin_FOTOTRANSISTOR_DIR) == GPIO.HIGH and GPIO.input(pin_FOTOTRANSISTOR_ESQ) == GPIO.LOW:
-		print("Direito Branco.")
-	elif GPIO.input(pin_FOTOTRANSISTOR_DIR) == GPIO.LOW and GPIO.input(pin_FOTOTRANSISTOR_ESQ) == GPIO.HIGH:
-		print("Esquerdo Branco.")
-	else:
-		print("Contencao.")
 
-	time.sleep(1)
+def fototransistores():
+	# Chamada da interface i2c
+	i2c = busio.I2C(board.SCL, board.SDA)
+
+	# Criando um objeto ADC com a interface i2c
+	ads = ADS.ADS1115(i2c)
+
+	# Criando duas entradas para leitura dos canais 0 e 1 do conversor AD
+	fototransistor_dir = AnalogIn(ads, ADS.P0)
+	fototransistor_esq = AnalogIn(ads, ADS.P1)
+
+	return fototransistor_dir.value, fototransistor_dir.voltage, fototransistor_esq.value, fototransistor_esq.voltage
+
 	
