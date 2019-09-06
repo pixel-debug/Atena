5b4f5450b84cb4a9cae23f9b07c107c2b5839120
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-06 05:49:20 -0300

    Substituído TCRT5000 por QRE113, e alteração da nomenclatura das variáveis.

--- Scripts Testes/teste_fototransistor.py
+++ Scripts Testes/teste_fototransistor.py
@@ -25,10 +25,12 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 
 # Criando duas entradas para leitura dos canais 0 e 1 do conversor AD
-fototransistor_dir = AnalogIn(ads, ADS.P0)
-fototransistor_esq = AnalogIn(ads, ADS.P1)
+ft_dir_extrem = AnalogIn(ads, ADS.P0)
+ft_dir_centro = AnalogIn(ads, ADS.P1)
+ft_esq_centro = AnalogIn(ads, ADS.P2)
+ft_esq_extrem = AnalogIn(ads, ADS.P3)
 
 # 1º valor raw e 2º valor da tensão
 while True:
-    print("Fototransistor Direita: {:>5}\t{:>5.3f} \tFototransistor Esquerda: {:>5}\t{:>5.3f}".format(fototransistor_dir.value, fototransistor_dir.voltage, fototransistor_esq.value, fototransistor_esq.voltage))
+    print("A0: {:>5}\tA1: {:>5} \tA2: {:>5}\tA3: {:>5}".format(ft_dir_extrem.value, ft_dir_centro.value, ft_esq_centro.value, ft_esq_extrem.value))
     time.sleep(0.5)
