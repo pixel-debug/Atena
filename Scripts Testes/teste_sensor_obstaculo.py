a18fbf8948bfcf984f0a5f9ae0a13b57e8b1bdc9
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-06 06:05:16 -0300

    Implementando sensor VL530X.

--- Scripts Testes/teste_sensor_obstaculo.py
+++ Scripts Testes/teste_sensor_obstaculo.py
@@ -15,40 +15,10 @@
 
 import RPi.GPIO as GPIO
 import time
+import board
+import busio
+
+import adafruit_vl53l0x
 
 
-GPIO.setmode(GPIO.BCM)
 
-GPIO.setwarnings(False)
-
-pin_ECHO = 23
-pin_TRIG = 24
-
-
-GPIO.setup(pin_TRIG, GPIO.OUT)
-GPIO.setup(pin_ECHO, GPIO.IN)
-
-GPIO.output(pin_TRIG, False)
-print("Esperando sensor")
-time.sleep(2)
-	
-GPIO.output(pin_TRIG,True)
-time.sleep(0.00001)
-GPIO.output(pin_TRIG, False)
-
-while GPIO.input(pin_ECHO) == 0:
-	pulse_start = time.time()
-
-while GPIO.input(pin_ECHO) == 1:
-	pulse_end = time.time()
-
-pulse_duration = pulse_end - pulse_start
-
-distance = pulse_duration * 17150
-
-distance = round(distance, 2)
-
-print("Distancia:", distance," cm")
-
-
-GPIO.cleanup()
