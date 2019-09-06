63b9552f5af2f6b88aafee9e88d21977738eb495
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-06 06:10:20 -0300

    Criação do loop e impressão da distancia detectada pelo sensor.

--- Scripts Testes/teste_sensor_obstaculo.py
+++ Scripts Testes/teste_sensor_obstaculo.py
@@ -25,3 +25,7 @@ i2c = busio.I2C(board.SCL, board.SDA)
 
 # Ativacao do sensor
 vl53 = adafruit_vl53l0x.VL53L0X(i2c)
+
+while True:
+    print('Distancia: {0}mm'.format(vl53.range))
+    time.sleep(1.0)
