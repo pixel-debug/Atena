e7369b7e36ac8aca9c9e55c4b2c34bc2908e5cc4
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-06 06:30:08 -0300

    Criando entradas para leitura dos canais A0, A1, A2, e A3 do conversor AD.

--- Funcoes.py
+++ Funcoes.py
@@ -35,10 +35,12 @@ def fototransistores():
 	# Criando um objeto ADC com a interface i2c
 	ads = ADS.ADS1115(i2c)
 
-	# Criando duas entradas para leitura dos canais 0 e 1 do conversor AD
-	fototransistor_dir = AnalogIn(ads, ADS.P0)
-	fototransistor_esq = AnalogIn(ads, ADS.P1)
+	# Criando entradas para leitura dos canais A0, A1, A2, e A3 do conversor AD
+	ft_dir_extrem = AnalogIn(ads, ADS.P0)
+	ft_dir_centro = AnalogIn(ads, ADS.P1)
+	ft_esq_centro = AnalogIn(ads, ADS.P2)
+	ft_esq_extrem = AnalogIn(ads, ADS.P3)
 
-	return fototransistor_dir.value, fototransistor_esq.value
+	return ft_dir_extrem.value, ft_dir_centro.value, ft_esq_centro.value, ft_esq_extrem.value
 
 	
