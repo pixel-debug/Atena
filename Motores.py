2d4def98a133ea7ffe8f359defb62e0faf68ee3a
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-07 12:35:27 -0300

    Retirado parâmetro de velocidade. A referência de velocidade é passada direta para cada função do script.

--- Motores.py
+++ Motores.py
@@ -15,24 +15,24 @@
 import RPi.GPIO as GPIO
 import Variaveis as var
 
-def movimento_frente(velocidade, controle_velocidade_direita, controle_velocidade_esquerda):
+def movimento_frente(controle_velocidade_direita, controle_velocidade_esquerda):
 	GPIO.output(var.pin_IN1, True)
 	GPIO.output(var.pin_IN2, False)
-	controle_velocidade_direita.ChangeDutyCycle(velocidade)
+	controle_velocidade_direita.ChangeDutyCycle(var.velocidade)
 
 	GPIO.output(var.pin_IN3, True)
 	GPIO.output(var.pin_IN4, False)
-	controle_velocidade_esquerda.ChangeDutyCycle(velocidade)
+	controle_velocidade_esquerda.ChangeDutyCycle(var.velocidade)
 
 
-def movimento_tras(velocidade, controle_velocidade_direita, controle_velocidade_esquerda):
+def movimento_tras(controle_velocidade_direita, controle_velocidade_esquerda):
 	GPIO.output(var.pin_IN1, False)
 	GPIO.output(var.pin_IN2, True)
-	controle_velocidade_direita.ChangeDutyCycle(velocidade)
+	controle_velocidade_direita.ChangeDutyCycle(var.velocidade)
 
 	GPIO.output(var.pin_IN3, False)
 	GPIO.output(var.pin_IN4, True)
-	controle_velocidade_esquerda.ChangeDutyCycle(velocidade)
+	controle_velocidade_esquerda.ChangeDutyCycle(var.velocidade)
 
 
 def parar_movimento(controle_velocidade_direita, controle_velocidade_esquerda):
@@ -45,22 +45,22 @@ def parar_movimento(controle_velocidade_direita, controle_velocidade_esquerda):
 	controle_velocidade_esquerda.ChangeDutyCycle(0)
 
 
-def movimento_esquerda(velocidade, controle_velocidade_direita, controle_velocidade_esquerda):
+def movimento_esquerda(controle_velocidade_direita, controle_velocidade_esquerda):
 	GPIO.output(var.pin_IN1, True)
 	GPIO.output(var.pin_IN2, False)
-	controle_velocidade_direita.ChangeDutyCycle(velocidade)
+	controle_velocidade_direita.ChangeDutyCycle(var.velocidade + var.vel_correcao_dir)
 
 	GPIO.output(var.pin_IN3, False)
 	GPIO.output(var.pin_IN4, True)
-	controle_velocidade_esquerda.ChangeDutyCycle(velocidade)
+	controle_velocidade_esquerda.ChangeDutyCycle(var.velocidade)
 
 
-def movimento_direita(velocidade, controle_velocidade_direita, controle_velocidade_esquerda):
+def movimento_direita(controle_velocidade_direita, controle_velocidade_esquerda):
 	GPIO.output(var.pin_IN1, False)
 	GPIO.output(var.pin_IN2, True)
-	controle_velocidade_direita.ChangeDutyCycle(velocidade)
+	controle_velocidade_direita.ChangeDutyCycle(var.velocidade)
 	GPIO.output(var.pin_IN3, True)
 	GPIO.output(var.pin_IN4, False)
-	controle_velocidade_esquerda.ChangeDutyCycle(velocidade)
+	controle_velocidade_esquerda.ChangeDutyCycle(var.velocidade + var.vel_correcao_esq)
 
 
