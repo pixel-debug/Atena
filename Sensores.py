7bab2bbb3c1f597259ce9385e2f6ea12e28ea9b9
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-07 12:33:36 -0300

    Definido nome das funções como o nome real dos sensores utilizados.

--- Sensores.py
+++ Sensores.py
@@ -14,7 +14,6 @@
 
 import RPi.GPIO as GPIO
 import Variaveis as var
-import time
 import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
@@ -24,18 +23,17 @@ import adafruit_vl53l0x
 # inicializacao do protocolo i2c
 i2c = busio.I2C(board.SCL, board.SDA)
 
-def buzina(ativa_buzina):
+
+def buzzer(ativa_buzina):
 	if ativa_buzina == True:	
 		GPIO.output(var.pin_BUZINA, True)
 	else:
 		GPIO.output(var.pin_BUZINA, False)
 		
 
-def sensor_fototransistor():
-	# Criando um objeto ADC com a interface i2c
+def fototransistores():
 	ads = ADS.ADS1115(i2c)
 
-	# Criando entradas para leitura dos canais A0, A1, A2, e A3 do conversor AD
 	ft_dir_extrem = AnalogIn(ads, ADS.P0)
 	ft_dir_centro = AnalogIn(ads, ADS.P1)
 	ft_esq_centro = AnalogIn(ads, ADS.P2)
@@ -44,11 +42,12 @@ def sensor_fototransistor():
 	return ft_dir_extrem.value, ft_dir_centro.value, ft_esq_centro.value, ft_esq_extrem.value
 
 
-def sensor_obstaculo():
-	# Ativacao do sensor
+def vl530x():
 	vl53 = adafruit_vl53l0x.VL53L0X(i2c)
 
 	return vl53.range
 
+
+	
 	
 	
