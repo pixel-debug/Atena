8ea2f2579838604e7a8be7d1874cfe4d1d3dbd16
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-07 10:12:10 -0300

    Criado função para tratamento e detecção das faixas da pista.

--- Tratamento.py
+++ Tratamento.py
@@ -11,3 +11,15 @@
 #	Script: Tratamento
 
 # --------------------------------------------------------
+
+def deteccao_faixas_pista(ft_dir_extrem, ft_dir_centro, ft_esq_centro, ft_esq_extrem):
+	if (ft_dir_extrem < var.limite_ft_dir_extrem):
+		detectou_faixa_dir = True
+	elif ((ft_dir_centro < var.limite_ft_dir_centro) and (ft_esq_centro < var.limite_ft_esq_centro)):
+		detectou_faixas_centro = True
+	elif (ft_esq_extrem < var.limite_ft_esq_extrem):
+		res_faixa_esq = True
+	else:
+		detectou_faixa_dir, detectou_faixa_dir, detectou_faixa_esq =  False, False, False
+		
+	return detectou_faixa_dir, detectou_faixas_centro, detectou_faixa_esq
