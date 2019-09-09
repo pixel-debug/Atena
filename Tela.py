adbe98cb002c5328cc4b07aa37a8b8c5ced10873
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-09 18:58:07 -0300

    Alterado nome função apresenta_imagem para apresenta.

--- Tela.py
+++ Tela.py
@@ -16,8 +16,8 @@ import cv2
 import Variaveis as var
 
 
-def apresenta_imagem(nome, imagem, pos_x, pos_y):
+def apresenta(nome, imagem, pos_x, pos_y):
 	cv2.namedWindow(nome, cv2.WINDOW_KEEPRATIO);
 	cv2.moveWindow(nome, pos_x, pos_y);
-	cv2.resizeWindow(nome, var.tamanho_tela_x, var.tamanho_tela_y)
+	cv2.resizeWindow(nome, var.tam_mini_tela_x, var.tam_mini_tela_y)
 	cv2.imshow(nome, imagem)
