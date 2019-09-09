151a1c12d0e0cf957e85a2279747e04e3759fc98
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-09 15:38:55 -0300

    Criado laço de repetição e armazenamento dos frames no vetor imagem.

--- Pista.py
+++ Pista.py
@@ -24,4 +24,11 @@ camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(720, 560))
 time.sleep(0.1)
 
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+	# O vetor com os frames capturados sao armazenados no vetor image	
+	image = frame.array
+	
+	
+
+cv2.destroyAllWindows()
 
