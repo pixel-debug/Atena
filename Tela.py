c119fa9d3f5ac9ff01d718775a8b5c0850717e74
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-09 15:57:42 -0300

    Criado função de apresentação de imagens com a resolução de 480x320.

--- Tela.py
+++ Tela.py
@@ -11,3 +11,13 @@
 #	Script: Tela
 
 # --------------------------------------------------------
+
+import cv2
+import Variaveis as var
+
+
+def apresenta_imagem(nome, imagem, pos_x, pos_y):
+	cv2.namedWindow(nome, cv2.WINDOW_KEEPRATIO);
+	cv2.moveWindow(nome, pos_x, pos_y);
+	cv2.resizeWindow(nome, var.tamanho_tela_x, var.tamanho_tela_y)
+	cv2.imshow(nome, imagem)
