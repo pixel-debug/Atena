32d9d2aa0bbd136ce09e2e5fbadfbe3c0b48ddaf
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-10 18:39:01 -0300

    Testando tratamento dos fototransistores.

--- Tratamento.py
+++ Tratamento.py
@@ -13,19 +13,15 @@
 # --------------------------------------------------------
 
 import Variaveis as var
-import Sensores as sensor
+
 import Pista as pista
+import Tela as tela
 
-# Obtendo valores brutos dos sensores
-ft_dir_extrem, ft_dir_centro, ft_esq_centro, ft_esq_extrem = sensor.fototransistores()
-distancia_obstaculo = sensor.vl530x()			
-
-#print(ft_dir_extrem, ft_dir_centro, ft_esq_centro, ft_esq_extrem)
-
-
+'''
 def deteccao_faixas_pista(img):
 	detectou_faixa_dir, detectou_faixa_centro, detectou_faixa_esq = False, False, False	
 	img_perspectiva_pista = pista.perspectiva_pista(img)
+	tela.apresenta("Imagem Original", img_perspectiva_pista, 20, 20)
 	img_filtros = pista.aplicacao_filtros(img_perspectiva_pista) 
 
 	img_faixa_esq = img_filtros[var.y1_faixa_esq:var.y2_faixa_esq, var.x1_faixa_esq:var.x2_faixa_esq]
@@ -39,21 +35,21 @@ def deteccao_faixas_pista(img):
 	elif cx_esq <= 35:
 		detectou_faixa_esq = True
 	return detectou_faixa_dir, detectou_faixa_centro, detectou_faixa_esq
-
-
 '''
-def deteccao_faixas_pista():
+
+
+def deteccao_faixas_pista(ft_dir_extrem, ft_dir_centro, ft_esq_centro, ft_esq_extrem):
 	detectou_faixa_dir, detectou_faixa_centro, detectou_faixa_esq = False, False, False	
-	if ((ft_dir_extrem < var.limite_ft_dir_extrem) or (ft_dir_centro < var.limite_ft_dir_centro)):
+	if (ft_dir_extrem < var.limite_ft_dir_extrem):
 		detectou_faixa_dir = True
-	elif ((ft_dir_centro < var.limite_ft_dir_centro) and (ft_esq_centro < var.limite_ft_esq_centro)):
-		detectou_faixa_centro = True
+	#elif ((ft_dir_centro < var.limite_ft_dir_centro) and (ft_esq_centro < var.limite_ft_esq_centro)):
+	#	detectou_faixa_centro = True
 	elif (ft_esq_extrem < var.limite_ft_esq_extrem):
 		detectou_faixa_esq = True
 	return detectou_faixa_dir, detectou_faixa_centro, detectou_faixa_esq
-'''
 
-def deteccao_obstaculo():
+
+def deteccao_obstaculo(distancia_obstaculo):
 	if((distancia_obstaculo >= var.limite_obstaculo_incial) and (distancia_obstaculo <= var.limite_obstaculo_final)):
 		detectou_obstaculo = True
 	else:
