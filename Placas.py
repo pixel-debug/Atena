7eebc61d22aa00976d8147b7a70796ecfe638771
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-11 19:28:53 -0300

    Importado bibliotecas e feito configurações da camera.

--- Placas.py
+++ Placas.py
@@ -11,3 +11,15 @@
 #	Script: Placas
 
 # --------------------------------------------------------
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import RPi.GPIO as GPIO
+import time
+import cv2
+
+# Inicializacao e configuracao da camera 
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
