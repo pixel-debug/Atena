8e7d8974b382c715a57d6b6957f184862571238f
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-12 20:17:47 -0300

    Adicionando tratamento para detecção das placas no script Tratamento.

--- Tratamento.py
+++ Tratamento.py
@@ -15,13 +15,13 @@
 import Variaveis as var
 
 import Pista as pista
+import Placas as placa
 import Tela as tela
 
-'''
+
 def deteccao_faixas_pista(img):
 	detectou_faixa_dir, detectou_faixa_centro, detectou_faixa_esq = False, False, False	
 	img_perspectiva_pista = pista.perspectiva_pista(img)
-	tela.apresenta("Imagem Original", img_perspectiva_pista, 20, 20)
 	img_filtros = pista.aplicacao_filtros(img_perspectiva_pista) 
 
 	img_faixa_esq = img_filtros[var.y1_faixa_esq:var.y2_faixa_esq, var.x1_faixa_esq:var.x2_faixa_esq]
@@ -30,14 +30,17 @@ def deteccao_faixas_pista(img):
 	img_faixa_dir = img_filtros[var.y1_faixa_dir:var.y2_faixa_dir, var.x1_faixa_dir:var.x2_faixa_dir]
 	img_faixa_dir, cx_dir = pista.detecta_faixas(img_faixa_dir)
 
+	#tela.apresenta("Imagem Perspe", img_perspectiva_pista, 500, 10)
+	#tela.apresenta("Imagem Faixa Esquerda", img_faixa_esq, 10, 400)
+	#tela.apresenta("Imagem Faixa Direita", img_faixa_dir, 500, 400)
 	if cx_dir >= 50:
 		detectou_faixa_dir = True
 	elif cx_esq <= 35:
 		detectou_faixa_esq = True
 	return detectou_faixa_dir, detectou_faixa_centro, detectou_faixa_esq
+
+
 '''
-
-
 def deteccao_faixas_pista(ft_dir_extrem, ft_dir_centro, ft_esq_centro, ft_esq_extrem):
 	detectou_faixa_dir, detectou_faixa_centro, detectou_faixa_esq = False, False, False	
 	if (ft_dir_extrem < var.limite_ft_dir_extrem):
@@ -47,7 +50,7 @@ def deteccao_faixas_pista(ft_dir_extrem, ft_dir_centro, ft_esq_centro, ft_esq_ex
 	elif (ft_esq_extrem < var.limite_ft_esq_extrem):
 		detectou_faixa_esq = True
 	return detectou_faixa_dir, detectou_faixa_centro, detectou_faixa_esq
-
+'''
 
 def deteccao_obstaculo(distancia_obstaculo):
 	if((distancia_obstaculo >= var.limite_obstaculo_incial) and (distancia_obstaculo <= var.limite_obstaculo_final)):
@@ -55,6 +58,10 @@ def deteccao_obstaculo(distancia_obstaculo):
 	else:
 		detectou_obstaculo = False
 	return detectou_obstaculo
+
+
+def deteccao_placa(img):
+	img_reigao_placa = img[var.y1_img_placas_dir:var.y2_img_placas_dir, var.x1_img_placas_dir:var.x2_img_placas_dir]
 	
 	
 		   
