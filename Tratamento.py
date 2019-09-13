9c5ad73fd9cf919a1f2a9ef9045f46fa761534e7
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-13 17:53:43 -0300

    Alterando tratamento para detecção de obstaculos.

--- Tratamento.py
+++ Tratamento.py
@@ -17,6 +17,7 @@ import Variaveis as var
 import Pista as pista
 #import Placas as placa
 import Tela as tela
+import cv2
 
 '''
 def deteccao_faixas_pista(img):
@@ -42,12 +43,40 @@ def deteccao_faixas_pista(img):
 '''
 
 
+
+'''
+	detectou_faixa_dir_visao, detectou_faixa_esq_visao = False, False
+
+	img_perspectiva_pista = pista.perspectiva_pista(img)
+	img_filtros = pista.aplicacao_filtros(img_perspectiva_pista) 
+
+	img_faixa_esq = img_filtros[var.y1_faixa_esq:var.y2_faixa_esq, var.x1_faixa_esq:var.x2_faixa_esq]
+	img_faixa_esq, cx_esq = pista.detecta_faixas(img_faixa_esq)
+
+	img_faixa_dir = img_filtros[var.y1_faixa_dir:var.y2_faixa_dir, var.x1_faixa_dir:var.x2_faixa_dir]
+	img_faixa_dir, cx_dir = pista.detecta_faixas(img_faixa_dir)
+
+	tela.apresenta("Imagem Original", img, 10, 10)
+	#cv2.imshow("Imagem Original", img)
+	#tela.apresenta("Imagem Perspe", img_perspectiva_pista, 500, 10)
+	#tela.apresenta("Imagem Faixa Esquerda", img_faixa_esq, 10, 400)
+	#tela.apresenta("Imagem Faixa Direita", img_faixa_dir, 500, 400)	
+	
+
+	if cx_dir >= 70:
+		detectou_faixa_dir_visao = True
+	elif cx_esq <= 45:
+		detectou_faixa_esq_visao = True
+
+'''
+
+
 def deteccao_faixas_pista(ft_dir_ext, ft_dir_cen, ft_esq_cen, ft_esq_ext):
 	detectou_faixa_dir_ext, detectou_faixa_dir_cen = False, False 
-	detectou_faixa_esq_cen, detectou_faixa_esq_ext = False, False	
-	
+	detectou_faixa_esq_cen, detectou_faixa_esq_ext = False, False
+
 	if (ft_dir_ext < var.CONST_FT_DIR_EXT):
-		detectou_faixa_dir = True
+		detectou_faixa_dir_ext = True
 
 	elif (ft_dir_cen < var.CONST_FT_DIR_CEN):
 		detectou_faixa_dir_cen = True
@@ -56,14 +85,13 @@ def deteccao_faixas_pista(ft_dir_ext, ft_dir_cen, ft_esq_cen, ft_esq_ext):
 		detectou_faixa_esq_cen = True
 
 	elif (ft_esq_ext < var.CONST_FT_ESQ_EXT):
-		detectou_faixa_esq = True
-	
-	
-	return detectou_faixa_dir_ext, detectou_faixa_dir_cen, detectou_faixa_esq_cen, detectou_faixa_esq_ext
+		detectou_faixa_esq_ext = True
 
+	return detectou_faixa_dir_ext, detectou_faixa_dir_cen, detectou_faixa_esq_cen, detectou_faixa_esq_ext
+			
 
 def deteccao_obstaculo(distancia_obstaculo):
-	if((distancia_obstaculo >= var.limite_obstaculo_incial) and (distancia_obstaculo <= var.limite_obstaculo_final)):
+	if((distancia_obstaculo >= var.CONST_OBSTAC_INI) and (distancia_obstaculo <= var.CONST_OBSTAC_FIM)):
 		detectou_obstaculo = True
 	else:
 		detectou_obstaculo = False
