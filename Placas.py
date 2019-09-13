1938b9428374a1d1f54209927d2e6cc2385f4ed3
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-12 21:21:36 -0300

    Testando vetor de classificadores.

--- Placas.py
+++ Placas.py
@@ -25,7 +25,7 @@ camera.resolution = (var.tam_original_tela_x, var.tam_original_tela_y)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(var.tam_original_tela_x, var.tam_original_tela_y))
 
-def detecta_placa(nome, img, classificador):
+def detecta_placas(img, classificador):
 	img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 	placa_detectada = classificador.detectMultiScale(img_gray, 1.1, 5)
@@ -36,7 +36,7 @@ def detecta_placa(nome, img, classificador):
 	for (x,y,w,h) in placa_detectada:
 		distancia_placa = calculo_distancia_placa(x, w)
 
-	return img, distancia_placa
+	return img
 
 def calculo_distancia_placa(x, w):
 	return int((-0.26316) * ((x + w)-x) + 45.78947)
@@ -47,7 +47,14 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 
 	reigao_placa = imagem[var.y1_img_placas_dir:var.y2_img_placas_dir, var.x1_img_placas_dir:var.x2_img_placas_dir]
 
-	imagem_placas = detecta_placa("Pare", reigao_placa, var.classificador_pare)
+	for (n, c) in var.classificadores:
+		imagem_placas = detecta_placas(reigao_placa, c)
+		print(n)
+	#imagem_p2 = detecta_placas(var.nome_p2, reigao_placa, var.classificador_p2)
+
+	#imagem_p3 = detecta_placas(var.nome_p3, reigao_placa, var.classificador_p3)
+
+
 
 	# Apresenta imagem
 	cv2.imshow("Frame", imagem)
