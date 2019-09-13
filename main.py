82ba99f0d0bfa375113dfaed3573ed605dd277be
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-13 11:19:22 -0300

    Teste de correção dos motores com while. FUNCIONANDO.

--- main.py
+++ main.py
@@ -51,38 +51,58 @@ class main:
 	try:
 		for frame in frames.capture_continuous(capturaFrames, format="bgr", use_video_port=True):
 			imagem = frame.array
-
+			#tela.apresenta("Imagem Original", imagem, 10, 10)
+			#cv2.imshow("Imagem Original", imagem)
+	
+			# ------------------- Obtencao valores sensores ----------------------
 			# Obtendo valores brutos dos sensores
 			ft_dir_ex, ft_dir_ce, ft_esq_ce, ft_esq_ex = sensor.fototransistores()
 
-			print(ft_dir_ex, ft_dir_ce, ft_esq_ce, ft_esq_ex)
+			#print(ft_dir_ex, ft_dir_ce, ft_esq_ce, ft_esq_ex)
 			distancia_obstaculo = sensor.vl530x()	
+			# -------------------------------------------------------------------
 
-			#tela.apresenta("Imagem Original", imagem, 20, 20)
 
+			# ---------------- Resultados dos Tratamentos -----------------------
 			# Deteccao das faixas na pista
 			deteccao_faixa_dir, deteccao_faixa_centro, deteccao_faixa_esq = trata.deteccao_faixas_pista(ft_dir_ex, ft_dir_ce, ft_esq_ce, ft_esq_ex)
+
 			#deteccao_faixa_dir, deteccao_faixa_centro, deteccao_faixa_esq = trata.deteccao_faixas_pista(imagem)
-			#print(deteccao_faixa_dir, deteccao_faixa_centro, deteccao_faixa_esq)	
+
 			# Deteccao de obstaculos na pista
 			deteccao_obstaculo = trata.deteccao_obstaculo(distancia_obstaculo)
+			
+			# Deteccao de placas na pista
+			#deteccao_placa = trata.deteccao_placa(imagem)
+			# -------------------------------------------------------------------
+
+			print(ft_dir_ex, ft_dir_ce, ft_esq_ce, ft_esq_ex)
+			#print(deteccao_faixa_dir, deteccao_faixa_centro, deteccao_faixa_esq)	
+			
+			
+			if (deteccao_faixa_dir is  True):
+				while(deteccao_faixa_dir is not False):
+					print("Virar Esquerda") 
+					#motor.movimento_esquerda(controle_velocidade_direita, controle_velocidade_esquerda)
+					deteccao_faixa_dir = False
+	
 
 
+
+			'''
 			if (deteccao_faixa_dir ==  True):
-				print("Virar Esquerda") 
+				#print("Virar Esquerda") 
 				motor.movimento_esquerda(controle_velocidade_direita, controle_velocidade_esquerda)
-				time.sleep(1.4)
 			elif (deteccao_faixa_esq  ==  True):
 				#print("Virar Direita")				
 				motor.movimento_direita(controle_velocidade_direita, controle_velocidade_esquerda)
-				time.sleep(1.4)
 			#elif ((deteccao_faixa_centro  ==  True) or (deteccao_obstaculo  ==  True)):
 				#print("Parar movimento")				
 				#motor.parar_movimento(controle_velocidade_direita, controle_velocidade_esquerda)
 			else:
 				#print("Seguir em frente")
 				motor.movimento_frente(controle_velocidade_direita, controle_velocidade_esquerda) 
-
+			'''
 			capturaFrames.truncate(0)
 			
 			if cv2.waitKey(1) & 0xFF == 27:
