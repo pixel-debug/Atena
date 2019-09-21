2d302933b02d99f64b2521798e1742593eeb392c
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-20 22:10:00 -0300

    Alterado nome das variáveis.

--- Scripts Testes/teste_fototransistor.py
+++ Scripts Testes/teste_fototransistor.py
@@ -25,12 +25,12 @@ i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 
 # Criando duas entradas para leitura dos canais 0 e 1 do conversor AD
-ft_dir_extrem = AnalogIn(ads, ADS.P0)
-ft_dir_centro = AnalogIn(ads, ADS.P1)
-ft_esq_centro = AnalogIn(ads, ADS.P2)
-ft_esq_extrem = AnalogIn(ads, ADS.P3)
+a0 = AnalogIn(ads, ADS.P0)
+a1 = AnalogIn(ads, ADS.P1)
+a2 = AnalogIn(ads, ADS.P2)
+a3 = AnalogIn(ads, ADS.P3)
 
 # 1º valor raw e 2º valor da tensão
 while True:
-    print("ft_dir_ext: {:>5}\tft_dir_cen: {:>5} \tft_esq_cen: {:>5}\tft_esq_ext: {:>5}".format(ft_dir_extrem.value, ft_dir_centro.value, ft_esq_centro.value, ft_esq_extrem.value))
+    print("A0:{:>5} \tA1:{:>5} \tA2:{:>5} \tA3:{:>5}".format(a0.value, a1.value, a2.value, a3.value))
     time.sleep(0.5)
