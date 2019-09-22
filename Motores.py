0d4847a938baffad4efa7d791f41e23dc90e03ab
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-22 06:00:58 -0300

    Adicionado parâmentro de velocidade nas funções de virar para a esquerda e virar para a direita, para para situações de velocidade de emergencia.

--- Motores.py
+++ Motores.py
@@ -45,22 +45,22 @@ def parar_movimento(controle_velocidade_direita, controle_velocidade_esquerda):
 	controle_velocidade_esquerda.ChangeDutyCycle(0)
 
 
-def movimento_direita(controle_velocidade_direita, controle_velocidade_esquerda):
+def movimento_direita(velocidade, controle_velocidade_direita, controle_velocidade_esquerda):
 	GPIO.output(var.pin_IN1, True)
 	GPIO.output(var.pin_IN2, False)
-	controle_velocidade_direita.ChangeDutyCycle(var.velocidade + var.vel_correcao_dir)
+	controle_velocidade_direita.ChangeDutyCycle(velocidade)
 
 	GPIO.output(var.pin_IN3, False)
 	GPIO.output(var.pin_IN4, True)
-	controle_velocidade_esquerda.ChangeDutyCycle(var.velocidade + (var.vel_correcao_esq * 0.75))
+	controle_velocidade_esquerda.ChangeDutyCycle((velocidade * 0.75))
 
 
-def movimento_esquerda(controle_velocidade_direita, controle_velocidade_esquerda):
+def movimento_esquerda(velocidade, controle_velocidade_direita, controle_velocidade_esquerda):
 	GPIO.output(var.pin_IN1, False)
 	GPIO.output(var.pin_IN2, True)
-	controle_velocidade_direita.ChangeDutyCycle(var.velocidade + (var.vel_correcao_dir * .75))
+	controle_velocidade_direita.ChangeDutyCycle((velocidade * 0.75))
 	GPIO.output(var.pin_IN3, True)
 	GPIO.output(var.pin_IN4, False)
-	controle_velocidade_esquerda.ChangeDutyCycle(var.velocidade + var.vel_correcao_esq)
+	controle_velocidade_esquerda.ChangeDutyCycle(velocidade)
 
 
