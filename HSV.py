9cdb375916af3e52fa95950b363c088cb7549e8b
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-23 23:01:56 -0300

    Importação bibliotecas e configuração da camera.

--- HSV.py
+++ HSV.py
@@ -11,3 +11,16 @@
 #	Script: HSV
 
 # --------------------------------------------------------
+
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+# Inicializacao da camera e parâmetros de resolucao e quadros por segundo capturado
+camera = PiCamera()
+camera.resolution = (720, 560)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(720, 560))
+time.sleep(0.1)
+
