8d6a92ce0e26869cd6086526bea0ba5e7c51a096
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-24 21:07:25 -0300

    Criado script para detecção de obstaculos na pista com visão computacional.

---
+++ Obstaculos.py
@@ -0,0 +1,15 @@
+#-*- coding utf-8 -*-
+
+# ------------------ Projeto Atena -----------------------
+
+# 	Autores: Estanislau Filho e José Antônio
+# 	Ano: 2019
+# 	Orientadora: Natália Batista
+#	Instituição: CEFET-MG	
+# 	Link github: https://github.com/EstanislauFilho/Atena
+
+#	Script: Obstaculos
+
+# --------------------------------------------------------
+
+
