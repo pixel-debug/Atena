b7d05a08326e4280fb3f0916a04abc80fa278346
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-24 22:31:44 -0300

    Alterado nome da função aplicacao_filtros para filtros_faixas().

--- Pista.py
+++ Pista.py
@@ -36,7 +36,7 @@ def perspectiva_pista(img):
 	
 
 
-def aplicacao_filtros(img):
+def filtros_faixas(img):
 	img_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	img_blur = cv2.GaussianBlur(img_cinza,(5,5),0)
 	
@@ -55,13 +55,13 @@ def aplicacao_filtros(img):
 def detecta_faixas(img):
 	# Color thresholding
 	ret,thresh = cv2.threshold(img,145,205,cv2.THRESH_BINARY_INV)
-
+	
 	# Find the contours of the frame
 	_, contours,hierarchy = cv2.findContours(thresh.copy(), 1, cv2.CHAIN_APPROX_NONE)
-
+	
 	if len(contours) > 0:
 		c = max(contours, key=cv2.contourArea)
-
+		
 		M = cv2.moments(c)
 
 		cx = int(M['m10']/M['m00'])
