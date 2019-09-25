71775073a0cb8a80be8e26b9279ae00dff92ad66
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-24 21:42:45 -0300

    Implmentando método aplicação filtros.

--- Obstaculos.py
+++ Obstaculos.py
@@ -29,3 +29,13 @@ def perspectiva_obstaculo(img):
 	matriz = cv2.getPerspectiveTransform(var.pontos_obstaculos, var.pontos_destino)
 	img = cv2.warpPerspective(img, matriz, (var.tam_original_tela_x, var.tam_original_tela_y)) 
 	return img
+
+
+def aplicacao_filtros(img):
+	img_cinza = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	img_blur = cv2.GaussianBlur(img_cinza,(5,5),0)
+
+	img_tresh = cv2.inRange(img_blur, var.tresh_min_obs, var.tresh_max_obs) 
+
+	return img_tresh
