66367778fa4afd657210bb92ab851720b9a3be64
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-24 21:14:43 -0300

    Criado script para detecção de obstaculos na pista com visão computacional.

--- Obstaculos.py
+++ Obstaculos.py
@@ -12,4 +12,17 @@
 
 # --------------------------------------------------------
 
+def perspectiva_pista(img):
+	cv2.line(img, var.pt_osbtaculo_1, var.pt_osbtaculo_2, (var.cor_azul), 4)
+	cv2.line(img, var.pt_osbtaculo_1, var.pt_osbtaculo_3, (var.cor_azul), 4)
+	cv2.line(img, var.pt_osbtaculo_2, var.pt_osbtaculo_4, (var.cor_azul), 4)
+	cv2.line(img, var.pt_osbtaculo_3, var.pt_osbtaculo_4, (var.cor_azul), 4)
 
+	cv2.line(img, var.pt_destino_1, var.pt_destino_2, (var.cor_verde), 4)
+	cv2.line(img, var.pt_destino_1, var.pt_destino_3, (var.cor_verde), 4)
+	cv2.line(img, var.pt_destino_2, var.pt_destino_4, (var.cor_verde), 4)
+	cv2.line(img, var.pt_destino_3, var.pt_destino_4, (var.cor_verde), 4)
+
+	matriz = cv2.getPerspectiveTransform(var.pontos_pista, var.pontos_destino)
+	img = cv2.warpPerspective(img, matriz, (var.tam_original_tela_x, var.tam_original_tela_y)) 
+	return img
