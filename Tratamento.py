59cffaf88853cc576ef8728c0254e7faf2d6d08a
EstanislauFilho <estanislau.sena@gmail.com>
2019-09-26 22:44:34 -0300

    Retirado funções placa_pare e placa_pedestre e alocados no script Gerenciador.py.

--- Tratamento.py
+++ Tratamento.py
@@ -12,18 +12,16 @@
 
 # --------------------------------------------------------
 
-import Variaveis as var
-import Motores as motor
+import cv2
 import time
+import numpy as np
+import Tela as tela
 import Pista as pista
 import Placas as placa
-import Tela as tela
-import cv2
-import RPi.GPIO as GPIO
+import Variaveis as var
+import Motores as motor
 import Sensores as sensor
-import Placas as placa
 import Interface as interface
-import numpy as np
 import Obstaculos as obstaculo
 
 
@@ -199,55 +197,5 @@ def deteccao_placas(img):
 	img = img_area_detecao_placa
 	return img, detectou_plc_pare, detectou_plc_pedestre, detectou_plc_desvio
 # ###############################################################################################
-
-
-def placa_pare(ctr_vel_motor_dir, ctr_vel_motor_esq):
-	tempoPare = 0
-	print("Detectou Placa Pare...")	
-	motor.parar_movimento(ctr_vel_motor_dir, ctr_vel_motor_esq)
-	time.sleep(4)
-	while(tempoPare <= var.tempoEsperaPlacaPare):
-		print("Andar ate nao ver placa pare")
-		motor.movimento_frente(var.velReacao, ctr_vel_motor_dir, ctr_vel_motor_esq)
-		tempoPare += 1
-	print("Saiu da placa pare")
-	
-	
-
-
-def placa_pedestre(ctr_vel_motor_dir, ctr_vel_motor_esq):
-	DETECCAO_FAIXA_PEDESTRE = False
-	deteccao_obstaculo = False
-	tempoPedestre = 0
-	print("Detectou Placa Pedestre! Andar cuidadosamente.")	
-	while(deteccao_obstaculo is not True):	
-		while(DETECCAO_FAIXA_PEDESTRE is not True):
-			a, b, c, d = sensor.fototransistores()
-			motor.movimento_frente(var.velNormal+2, ctr_vel_motor_dir, ctr_vel_motor_esq)			
-			if(b <= var.CONST_ft_dir_sup and c <= var.CONST_ft_esq_sup):
-				break
-			elif(a <= var.CONST_ft_dir_sup and d <= var.CONST_ft_esq_sup):
-				break
-
-		print("Chegou na Faixa! Aguarda 5 segundos...")
-		motor.parar_movimento(ctr_vel_motor_dir, ctr_vel_motor_esq)
-		time.sleep(5)
-
-		print("Verifica ausencia de pedestre...")
-		if(deteccao_obstaculo is False):
-			print("pode continuar...")
-			while(tempoPedestre <= var.tempoReacaoPlacaPedestre):
-				motor.movimento_frente(var.velReacao, ctr_vel_motor_dir, ctr_vel_motor_esq)
-				tempoPedestre += 1
-			break
-			
-		else:
-			print("presença de obstaculo confirmada...")
-			motor.parar_movimento(ctr_vel_motor_dir, ctr_vel_motor_esq)
-		
-
-
-
-
 
 
