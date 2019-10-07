e94bc3e46161e8a748cce73e2da7f90e17279a44
EstanislauFilho <estanislau.sena@gmail.com>
2019-10-06 21:54:57 -0300

    Retirado função de rotação da imagem.

--- Scripts Testes/camera.py
+++ Scripts Testes/camera.py
@@ -20,23 +20,15 @@ import cv2
 # Inicializacao da camera e parâmetros de resolucao e quadros por segundo capturado
 camera = PiCamera()
 camera.resolution = (720, 560)
-camera.framerate = 32
+camera.framerate = 90
 rawCapture = PiRGBArray(camera, size=(720, 560))
 time.sleep(0.1)
+
+#cont_frames = 0
 
 for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
 	# O vetor com os frames capturados sao armazenados no vetor image	
 	image = frame.array
-	
-	# Armazenamento das dimensoes padrao dos frames a serem capturados e criacao do vetor com os pontos centrais da imagem
-	(h, w) = image.shape[:2]
-	center = (w / 2, h / 2)
-
-	# Utilizacao da funcao RotationMatrix2D para realizar a rotacao da imagem a partir do ponto central
-	M = cv2.getRotationMatrix2D(center, 180, 1.0)
-
-	# Realiza a rotacao da imagem a partir das funcoes determinadas acima
-	rotacao = cv2.warpAffine(image, M, (w, h))
 
 	# Apresenta as imagens capturas por meio dos frames
 	cv2.imshow("Streaming Camera Atena", image)
@@ -44,7 +36,10 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	# Faz a limpeza do stream e faz a preparacao para a captura dos proximos frames
 	rawCapture.truncate(0)
 
-	# Se prescionar a letra 'q' sai do programa
+	#print(cont_frames)	
+
+	#cont_frames += 1	
+	# Se prescionar a tecla Esc sai do programa
 	if cv2.waitKey(1) & 0xFF == 27:
 		break
 
