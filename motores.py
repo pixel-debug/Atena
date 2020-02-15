68090a07494abee81bf60c9836c4bd66c2d20356
Estanislau Filho <estanislau.sena@gmail.com>
2020-02-15 18:42:09 -0300

    Script para os motores.

---
+++ motores.py
@@ -0,0 +1,33 @@
+#-*- coding utf-8 -*-
+
+# ----------------------- Atena --------------------------
+
+# 	Projeto: Navegação de um robô autônomo educativo 
+#            em cenário dinâmico 
+# 	Ano: 2020
+# 	Orientadora: Natalia Batista
+#	Instituição: CEFET-MG	
+# 	Link github: https://github.com/EstanislauFilho/Atena
+
+#	Classe: Motores
+
+# --------------------------------------------------------
+
+import RPi.GPIO as GPIO
+
+class Motores():
+	
+	def __init__(self):
+		pass
+
+	def seguir_Frente(velocidade, controle_motor_dir, controle_motor_esq):
+		pass
+	
+	def virar_Esq(velocidade, controle_motor_dir, controle_motor_esq):
+		pass
+
+	def virar_Dir(velocidade, controle_motor_dir, controle_motor_esq):
+		pass
+
+	def marcha_Re(velocidade, controle_motor_dir, controle_motor_esq):
+		pass
