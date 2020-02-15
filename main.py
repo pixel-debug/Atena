aa03e51fcd36d13c37a51de8f3db571a2eb946fa
Estanislau Filho <estanislau.sena@gmail.com>
2020-02-15 11:45:19 -0300

    Criado estrutura do programa principal.

---
+++ main.py
@@ -0,0 +1,26 @@
+#-*- coding utf-8 -*-
+
+# ----------------------- Atena --------------------------
+
+# 	Projeto: Navegação de um robô autônomo educativo 
+#            em cenário dinâmico 
+# 	Ano: 2020
+# 	Orientadora: Natalia Batista
+#	Instituição: CEFET-MG	
+# 	Link github: https://github.com/EstanislauFilho/Atena
+
+#	Classe: Main
+
+# --------------------------------------------------------
+
+class Main():
+	
+	def __inti__(self):
+		pass
+
+	def main(self):
+		pass
+
+if __name__ == '__main__':
+	execute_app = Main()
+	execute_app.main()
