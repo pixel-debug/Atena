758eda1593219cd4b0697d506ce0f5ecf82a913f
estanislau <estanislau.sena@gmail.com>
2020-04-03 20:58:06 -0300

    Definido pontos para criar o warpPerspective.

--- Modulo_Movimento_OpenCV/main_video.py
+++ Modulo_Movimento_OpenCV/main_video.py
@@ -8,14 +8,38 @@ Created on Thu Apr  2 20:44:38 2020
 
 
 import cv2
-
+import numpy as np
 video = cv2.VideoCapture("video.mp4")
+
+pt_pista_1, pt_pista_2, pt_pista_3, pt_pista_4 = (50,445), (695,445), (0,510), (735,510)
+
+
+pt_destino_1, pt_destino_2, pt_destino_3, pt_destino_4 = (150,0), (590,0), (150,680), (590,680)
+
+pontos_pista = np.float32([[pt_pista_1], [pt_pista_2], [pt_pista_3], [pt_pista_4]])
+pontos_destino = np.float32([[pt_destino_1], [pt_destino_2], [pt_destino_3], [pt_destino_4]])
+
+
+def perspectiva_pista(img):
+	cv2.line(img, pt_pista_1, pt_pista_2, (0,0,255), 4)
+	cv2.line(img, pt_pista_1, pt_pista_3, (0,0,255), 4)
+	cv2.line(img, pt_pista_2, pt_pista_4, (0,0,255), 4)
+	cv2.line(img, pt_pista_3, pt_pista_4, (0,0,255), 4)
+
+	cv2.line(img, pt_destino_1, pt_destino_2, (0,255,0), 4)
+	cv2.line(img, pt_destino_1, pt_destino_3, (0,255,0), 4)
+	cv2.line(img, pt_destino_2, pt_destino_4, (0,255,0), 4)
+	cv2.line(img, pt_destino_3, pt_destino_4, (0,255,0), 4)
+
+	#matriz = cv2.getPerspectiveTransform(var.pontos_pista, var.pontos_destino)
+	#img = cv2.warpPerspective(img, matriz, (var.tam_original_tela_x, var.tam_original_tela_y)) 
+	return img
 
 while(True):
     status, frame = video.read()
     
     # Redimensionamento da imagem
-    imagem = cv2.resize(frame, (400, 200))
+    imagem = cv2.resize(frame, (780, 520))
       
     imagem_cinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
     
@@ -27,12 +51,18 @@ while(True):
     
     imagem_final = cv2.add(imagem_tresh, imagem_canny)
     
-    #cv2.imshow("Imagem Original", imagem)  
+    
+    
+    imagem = perspectiva_pista(imagem)
+    
+    
+    
+    cv2.imshow("Imagem Original", imagem)  
     #cv2.imshow("Imagem Cinza", imagem_cinza)
     #cv2.imshow("Imagem Blur", imagem_blur)
-    cv2.imshow("Imagem Tresh", imagem_tresh)
-    cv2.imshow("Imagem Canny", imagem_canny)
-    cv2.imshow("Imagem Final", imagem_final)
+    #cv2.imshow("Imagem Tresh", imagem_tresh)
+    #cv2.imshow("Imagem Canny", imagem_canny)
+    #cv2.imshow("Imagem Final", imagem_final)
 
     if cv2.waitKey(1) & 0xFF == 27:
         break
