7651b5fa64889cc20fcdad2ec22b112e679e2b9d
estanislau <estanislau.sena@gmail.com>
2020-04-06 20:11:40 -0300

    Implmentação de uma função para detecção das placas.

--- Modulo_Placas/main_video.py
+++ Modulo_Placas/main_video.py
@@ -19,7 +19,19 @@ classificador_pedestre = cv2.CascadeClassifier('/home/estanislau/Projetos/Atena/
 
 classificador_desvio = cv2.CascadeClassifier('/home/estanislau/Projetos/Atena/Modulo_Placas/Classificadores/cascade_desvio.xml')
 
+classificadores = [classificador_pare, classificador_pedestre, classificador_desvio]
 
+def detecta_Placas(img, classificador):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    img_placa = classificador.detectMultiScale(img_gray, scaleFactor = 1.1, minNeighbors = 10)
+    
+    for (x,y,w,h) in img_placa:
+        if ((x + w) <= 200):
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
+            cv2.putText(img, "Placa", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 255), 2)
+
+    return img
 
 while(True):
     status, frame = video.read()
@@ -27,10 +39,9 @@ while(True):
     # Redimensionamento da imagem
     imagem = cv2.resize(frame, (680, 420))
     
-    imagem_gray = cv2.cvtColor(imagem,cv2.COLOR_BGR2GRAY)
-			
-    imagem_placa_pare = classificador_pare.detectMultiScale(imagem_gray, scaleFactor=1.1, minNeighbors=10)
+    imagem_placas = detecta_Placas(imagem, classificador_pare)
     
+    ''' 
     imagem_placa_pedestre = classificador_pedestre.detectMultiScale(imagem_gray, scaleFactor=1.1, minNeighbors=10)
     
     imagem_placa_desvio = classificador_desvio.detectMultiScale(imagem_gray, scaleFactor=1.1, minNeighbors=10)
@@ -46,10 +57,10 @@ while(True):
     for (x,y,w,h) in imagem_placa_desvio:
         cv2.rectangle(imagem,(x,y),(x+w,y+h),(255,0,0),2)
         cv2.putText(imagem, "Desvio", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)   
-        
+    '''    
     
     # Apresenta Imagens
-    cv2.imshow("Imagem", imagem)
+    cv2.imshow("Imagem Placas", imagem_placas)
     
     if cv2.waitKey(1) & 0xFF == 27:
         break
