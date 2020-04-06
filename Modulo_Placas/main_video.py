d78cbc172b0a50d4db520ec422e5ad1289d300e9
estanislau <estanislau.sena@gmail.com>
2020-04-05 21:59:04 -0300

    Importando classificadores.

--- Modulo_Placas/main_video.py
+++ Modulo_Placas/main_video.py
@@ -13,6 +13,13 @@ import numpy as np
 
 video = cv2.VideoCapture("video.mp4")
 
+classificador_pare = cv2.CascadeClassifier('/home/estanislau/Projetos/Atena/Modulo_Placas/Classificadores/cascade_pare.xml')
+
+classificador_pedestre = cv2.CascadeClassifier('/home/estanislau/Projetos/Atena/Modulo_Placas/Classificadores/cascade_pedestre.xml')
+
+classificador_desvio = cv2.CascadeClassifier('/home/estanislau/Projetos/Atena/Modulo_Placas/Classificadores/cascade_desvio.xml')
+
+
 
 while(True):
     status, frame = video.read()
@@ -20,6 +27,10 @@ while(True):
     # Redimensionamento da imagem
     imagem = cv2.resize(frame, (680, 420))
     
+    
+    
+    
+    # Apresenta Imagens
     cv2.imshow("Imagem", imagem)
     
     if cv2.waitKey(1) & 0xFF == 27:
