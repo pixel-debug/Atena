6f3aa1bb3456a1941654342f17a2c6f04961afb3
estanislau <estanislau.sena@gmail.com>
2020-04-05 22:33:11 -0300

    Teste com detecção das placas pare, pedestre e desvio.

--- Modulo_Placas/main_video.py
+++ Modulo_Placas/main_video.py
@@ -29,12 +29,24 @@ while(True):
     
     imagem_gray = cv2.cvtColor(imagem,cv2.COLOR_BGR2GRAY)
 			
-    imagem_placa = classificador_pare.detectMultiScale(imagem_gray, scaleFactor=1.1, minNeighbors=10)
+    imagem_placa_pare = classificador_pare.detectMultiScale(imagem_gray, scaleFactor=1.1, minNeighbors=10)
+    
+    imagem_placa_pedestre = classificador_pedestre.detectMultiScale(imagem_gray, scaleFactor=1.1, minNeighbors=10)
+    
+    imagem_placa_desvio = classificador_desvio.detectMultiScale(imagem_gray, scaleFactor=1.1, minNeighbors=10)
 	
-    for (x,y,w,h) in imagem_placa:
+    for (x,y,w,h) in imagem_placa_pare:
         cv2.rectangle(imagem,(x,y),(x+w,y+h),(255,0,0),2)
-        cv2.putText(imagem, "pare", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+        cv2.putText(imagem, "Pare", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
 		
+    for (x,y,w,h) in imagem_placa_pedestre:
+        cv2.rectangle(imagem,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.putText(imagem, "Pedestre", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)    
+      
+    for (x,y,w,h) in imagem_placa_desvio:
+        cv2.rectangle(imagem,(x,y),(x+w,y+h),(255,0,0),2)
+        cv2.putText(imagem, "Desvio", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)   
+        
     
     # Apresenta Imagens
     cv2.imshow("Imagem", imagem)
