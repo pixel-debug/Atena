270bc55394bfec649e5cf5eee9fe95793f1f21cf
estanislau <estanislau.sena@gmail.com>
2020-04-07 19:17:05 -0300

    Implementado apresentação das imagens de forma ordenada.

--- apresenta_frames_video.py
+++ apresenta_frames_video.py
@@ -10,26 +10,12 @@ Created on Mon Apr  6 21:40:46 2020
 import cv2
 import glob
 
-cont_imagem = 1
-vetor = []
-
-import re
-'''
-b = "A1B2C3"
-b = re.sub('[^0-9]', '', b)
-
-# 123
-print(b)
-'''
-
 print("Abrindo pasta Frames_Video/")
-for i in glob.glob('Frames_Video/*.jpg'):
-    i = int(re.sub('[^0-9]', '', i))    
-    print(i)
-    #imagem = cv2.imread(i)
+for i in sorted(glob.glob('Frames_Video/*.jpg')):  
+    #print(i)
+    imagem = cv2.imread(i)
     
-    vetor.append(i)
-	#cv2.imshow("Apresenta Imagens", imagem)
-	#cv2.waitKey(1000)
-vetor.sort()
+    cv2.imshow("Apresenta Imagens", imagem)
+    cv2.waitKey(1000)
+
 cv2.destroyAllWindows()	
