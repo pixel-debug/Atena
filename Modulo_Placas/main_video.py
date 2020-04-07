e23c2e3878b9c92da0f6c4f753fc6b2778d8f89c
estanislau <estanislau.sena@gmail.com>
2020-04-06 23:10:51 -0300

    Fazendo processamento de três classificadores simultaneamente

--- Modulo_Placas/main_video.py
+++ Modulo_Placas/main_video.py
@@ -21,17 +21,14 @@ classificador_desvio = cv2.CascadeClassifier('/home/estanislau/Projetos/Atena/Mo
 
 classificadores = [classificador_pare, classificador_pedestre, classificador_desvio]
 
-def detecta_Placas(img, classificador):
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def detecta_Placas(img_gray, classificador):
+    img_placa = classificador.detectMultiScale(img_gray, scaleFactor = 1.1, minNeighbors = 15)
     
-    img_placa = classificador.detectMultiScale(img_gray, scaleFactor = 1.1, minNeighbors = 10)
-    
-    for (x,y,w,h) in img_placa:
-        if ((x + w) <= 200):
-            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 255), 2)
-            cv2.putText(img, "Placa", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 255), 2)
+    for (x,y,w,h) in img_placa:       
+        cv2.rectangle(img_gray, (x, y), (x + w, y + h), (255, 0, 255), 2)
+        cv2.putText(img_gray, "Placa", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 255), 2)
 
-    return img
+    return img_gray
 
 while(True):
     status, frame = video.read()
@@ -39,26 +36,12 @@ while(True):
     # Redimensionamento da imagem
     imagem = cv2.resize(frame, (680, 420))
     
-    imagem_placas = detecta_Placas(imagem, classificador_pare)
+    imagem_gray = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
     
-    ''' 
-    imagem_placa_pedestre = classificador_pedestre.detectMultiScale(imagem_gray, scaleFactor=1.1, minNeighbors=10)
+    for i in classificadores:
+        imagem_placas = detecta_Placas(imagem_gray, i)
     
-    imagem_placa_desvio = classificador_desvio.detectMultiScale(imagem_gray, scaleFactor=1.1, minNeighbors=10)
-	
-    for (x,y,w,h) in imagem_placa_pare:
-        cv2.rectangle(imagem,(x,y),(x+w,y+h),(255,0,0),2)
-        cv2.putText(imagem, "Pare", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
-		
-    for (x,y,w,h) in imagem_placa_pedestre:
-        cv2.rectangle(imagem,(x,y),(x+w,y+h),(255,0,0),2)
-        cv2.putText(imagem, "Pedestre", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)    
-      
-    for (x,y,w,h) in imagem_placa_desvio:
-        cv2.rectangle(imagem,(x,y),(x+w,y+h),(255,0,0),2)
-        cv2.putText(imagem, "Desvio", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0, 0, 255), 2)   
-    '''    
-    
+
     # Apresenta Imagens
     cv2.imshow("Imagem Placas", imagem_placas)
     
